dc2c000f677dadf053913380fae8989b882d7c36
PardalV <145386771+PardalV@users.noreply.github.com>
2025-10-21 11:42:49 -0300

    Estrutura da arvore Binária

--- main.java
+++ main.java
@@ -8,6 +8,28 @@ public class No{
 		direita = null;
 	}
 }
+
+public class ArvoreBi{
+	private No raiz;
+	public ArvoreBi(){
+		this.raiz = null;
+	}
+	public void inserir(int valor){
+		raiz = inserirRecursivo(raiz, valor);
+	}
+	private No inserirRecursivo(No noAtual, int valor){
+		if(noAtual == null){
+			No rec = new No(valor);
+			return rec;
+			//Fazer um get la em No para o valor
+		} else if(valor < noAtual.info){
+			noAtual.esquerda = inserirRecursivo(noAtual.esquerda, valor);
+		} else if(valor > noAtual.info){
+			noAtual.direita = inserirRecursivo(noAtual.direita, valor);
+		}
+		return noAtual;
+	}
+}
 public class Main
 {
 	public static void main(String[] args) {
