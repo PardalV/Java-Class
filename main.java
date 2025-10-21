fcce6e6c64c06a78c086b43ac2458abde9801731
PardalV <145386771+PardalV@users.noreply.github.com>
2025-10-21 08:30:44 -0300

    Adição do No no código

--- main.java
+++ main.java
@@ -1,3 +1,13 @@
+public class No{
+	int info;
+	No esquerda;
+	No direita;
+	public No(int info){
+		this.info = info;
+		esquerda = null;
+		direita = null;
+	}
+}
 public class Main
 {
 	public static void main(String[] args) {
