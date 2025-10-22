b5557132309f2ea802e8109b27cf8bbbd8119df3
PardalV <145386771+PardalV@users.noreply.github.com>
2025-10-22 09:23:01 -0300

    Atualização de classes

--- No.java
+++ No.java
@@ -8,38 +8,3 @@ public class No{
 		direita = null;
 	}
 }
-
-class ArvoreBi{
-	private No raiz;
-	public ArvoreBi(){
-		this.raiz = null;
-	}
-	public void inserir(int valor){
-		raiz = inserirRecursivo(raiz, valor);
-	}
-	private No inserirRecursivo(No noAtual, int valor){
-		if(noAtual == null){
-			No rec = new No(valor);
-			return rec;
-			//Fazer um get la em No para o valor
-		} else if(valor < noAtual.info){
-			noAtual.esquerda = inserirRecursivo(noAtual.esquerda, valor);
-		} else if(valor > noAtual.info){
-			noAtual.direita = inserirRecursivo(noAtual.direita, valor);
-		}
-		return noAtual;
-	}
-	public void imprimirEmOrderm(){
-		System.out.println("Imprimindo em ordem crescente:");
-		imprimirEmOrdermRecursivo(this.raiz);
-		System.out.println();
-	}
-	private void imprimirEmOrdermRecursivo(No noAtual){
-		if(noAtual == null){
-			return;
-		}
-		imprimirEmOrdermRecursivo(noAtual.esquerda);
-		System.out.print(noAtual.info + " ");
-		imprimirEmOrdermRecursivo(noAtual.direita);
-	}
-}
