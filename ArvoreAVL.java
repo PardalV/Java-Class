490c22cbd920da4882118ed4257b340c0754bed7
PardalV <145386771+PardalV@users.noreply.github.com>
2025-10-22 17:58:05 -0300

    Atualização de rotação de AVL

--- ArvoreAVL.java
+++ ArvoreAVL.java
@@ -1,4 +1,7 @@
 public class ArvoreAVL implements IArvore{
+    private NoAVL raiz;
+
+    //Metodos que precisam de Override
     @Override
     public void inserir(int valor){
 
@@ -14,5 +17,45 @@ public class ArvoreAVL implements IArvore{
     @Override
     public void imprimir(){
         
+    }
+
+    //Métodos Auxiliares
+    public int getAltura(NoAVL no){
+        if(no == null){
+            return 0;
+        } else {
+            return no.altura;
+        }
+    }
+    public int getFatorBalanceamento(NoAVL no){
+        if(no == null){
+            return 0;
+        } else {
+            return getAltura(no.esquerda) - getAltura(no.direita);
+        }
+    }
+    public void atualizarAltura(NoAVL no){
+        no.altura = 1 + Math.max(getAltura(no.esquerda), getAltura(no.direita));
+    }
+
+    //Rotações 
+    public NoAVL rocacaoDireita(NoAVL y){
+        NoAVL aux = y;
+        NoAVL x = y.esquerda;
+        NoAVL k = x.direita;
+        y = x;
+        y.direita = aux;
+        aux.esquerda = k;
+    }
+}
+
+class NoAVL{
+    int info;
+    NoAVL esquerda, direita;
+    int altura;
+
+    public NoAVL(int info){
+        this.info = info;
+        this.altura = 1;
     }
 }
