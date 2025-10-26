8785f3b844e8895e153ec48ea67cc5b1804fb7b8
PardalV <145386771+PardalV@users.noreply.github.com>
2025-10-26 15:43:05 -0300

    Continuação da arvore Rubro-Negra

--- ArvoreRubroNegra.java
+++ ArvoreRubroNegra.java
@@ -5,7 +5,28 @@ public class ArvoreRubroNegra implements IArvore{
     }
     @Override
     public void inserir(int valor){
+        NoRB z = new NoRB(valor);
 
+        NoRB y = null;
+        NoRB x = this.raiz;
+        while (x != null){
+            y = x;
+            if(z.info < x.info){
+                x = x.esquerda;
+            } else {
+                x = x.direita;
+            }
+        }
+        z.pai = y;
+        if(y == null){
+            this.raiz = z;
+        } else if(z.info < y.info){
+            y.esquerda = z;
+        } else {
+            y.direita = z;
+        }
+        corrigirInsercao(z);
+        this.raiz.cor = Cor.PRETO;
     }
     @Override
     public void remover(int valor){
@@ -13,11 +34,17 @@ public class ArvoreRubroNegra implements IArvore{
     }
     @Override
     public boolean buscar(int valor){
-
+        return buscarRecursivo(this.raiz, valor);
     }
     @Override
     public void imprimir(){
-        
+        System.out.println("--- Estrutura da Árvore R-N ---");
+    if (raiz == null) {
+        System.out.println("[Árvore Vazia]");
+    } else {
+        imprimirEstruturaRecursivo(this.raiz, "", true);
+    }
+    System.out.println("---------------------------------");
     }
     private void rotacaoEsquerda(NoRB x){
         NoRB y = x.direita;
@@ -52,5 +79,88 @@ public class ArvoreRubroNegra implements IArvore{
         }
         x.direita = y;
         y.pai = x;
+    }
+    private Cor getCor(NoRB no){
+        if(no == null){
+            return Cor.PRETO;
+        }
+        return no.cor;
+    }
+
+    private void corrigirInsercao(NoRB z){
+        while (z != raiz && getCor(z.pai) == Cor.VERMELHO){
+            NoRB avo = z.pai.pai;
+            NoRB tio;
+
+            if(z.pai == avo.esquerda){
+                tio = avo.direita;
+                if(getCor(tio) == Cor.VERMELHO){
+                    z.pai.cor = Cor.PRETO;
+                    tio.cor = Cor.PRETO;
+                    avo.cor = Cor.VERMELHO;
+                    z = avo;
+                } else {
+                    if (z == z.pai.direita){
+                        z = z.pai;
+                        rotacaoEsquerda(z);
+                    }
+                    z.pai.cor = Cor.PRETO;
+                    z.pai.pai.cor = Cor.VERMELHO;
+                    rotacaoDireita(z.pai.pai);
+                }
+            } else {
+                tio = avo.esquerda;
+                if(getCor(tio) == Cor.VERMELHO){
+                    z.pai.cor = Cor.PRETO;
+                    tio.cor = Cor.PRETO;
+                    avo.cor = Cor.VERMELHO;
+                    z = avo;
+                } else {
+                    if(z == z.pai.esquerda){
+                        z = z.pai;
+                        rotacaoDireita(z);
+                    }
+                    z.pai.cor = Cor.PRETO;
+                    z.pai.pai.cor = Cor.VERMELHO;
+                    rotacaoEsquerda(z.pai.pai);
+                }
+            }
+        }
+    }
+    private boolean buscarRecursivo(NoRB noAtual, int valor){
+        if(noAtual == null){
+            return false;
+        }
+        if(valor == noAtual.info){
+            return true;
+        }
+        if(valor < noAtual.info){
+            return buscarRecursivo(noAtual.esquerda, valor);
+        } else {
+            return buscarRecursivo(noAtual.direita, valor);
+        }
+    }
+    private void imprimirEstruturaRecursivo(NoRB no, String indent, boolean isUltimo) {
+    if (no == null) {
+        
+        return;
+    }
+
+    
+    String cor = (no.cor == Cor.VERMELHO) ? "V" : "P";
+    
+    
+    System.out.print(indent);
+    if (isUltimo) {
+        System.out.print("└── ");
+        indent += "    "; 
+    } else {
+        System.out.print("├── ");
+        indent += "│   "; 
+    }
+    System.out.println(no.info + "(" + cor + ")");
+
+    imprimirEstruturaRecursivo(no.esquerda, indent, false);
+    imprimirEstruturaRecursivo(no.direita, indent, true);
     }
 }
