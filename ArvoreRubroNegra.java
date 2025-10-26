1b887c2ab10d8ef2438e18c333d3c85fce5deee3
PardalV <145386771+PardalV@users.noreply.github.com>
2025-10-26 15:59:49 -0300

    Finalização da arvore Rubro Negra

--- ArvoreRubroNegra.java
+++ ArvoreRubroNegra.java
@@ -30,7 +30,40 @@ public class ArvoreRubroNegra implements IArvore{
     }
     @Override
     public void remover(int valor){
-
+        NoRB z = encontrarNo(valor);
+        if(z == null){
+            return;
+        }
+        NoRB x;
+        NoRB y = z;
+        Cor corOriginalDeY = y.cor;
+        if (z.esquerda == null) {
+        x = z.direita;
+        transplantar(z, z.direita);
+        } else if (z.direita == null) {
+            x = z.esquerda;
+            transplantar(z, z.esquerda);
+        } else {
+            y = encontrarMenorValor(z.direita); 
+            corOriginalDeY = y.cor;
+            x = y.direita; 
+            if (y.pai == z) {
+                if (x != null) {
+                    x.pai = y; 
+                }
+            } else {
+                transplantar(y, y.direita);
+                y.direita = z.direita;
+                y.direita.pai = y;
+            }
+            transplantar(z, y);
+            y.esquerda = z.esquerda;
+            y.esquerda.pai = y;
+            y.cor = z.cor; 
+        }
+        if (corOriginalDeY == Cor.PRETO) {
+            corrigirRemocao(x);
+        }
     }
     @Override
     public boolean buscar(int valor){
@@ -141,26 +174,109 @@ public class ArvoreRubroNegra implements IArvore{
         }
     }
     private void imprimirEstruturaRecursivo(NoRB no, String indent, boolean isUltimo) {
-    if (no == null) {
-        
-        return;
-    }
+        if (no == null) {
+            return;
+        }
+        String cor = (no.cor == Cor.VERMELHO) ? "V" : "P";
 
-    
-    String cor = (no.cor == Cor.VERMELHO) ? "V" : "P";
-    
-    
-    System.out.print(indent);
-    if (isUltimo) {
-        System.out.print("└── ");
-        indent += "    "; 
-    } else {
-        System.out.print("├── ");
-        indent += "│   "; 
+        System.out.print(indent);
+        if (isUltimo) {
+            System.out.print("└── ");
+            indent += "    "; 
+        } else {
+            System.out.print("├── ");
+            indent += "│   "; 
+        }
+        System.out.println(no.info + "(" + cor + ")");
+        imprimirEstruturaRecursivo(no.esquerda, indent, false);
+        imprimirEstruturaRecursivo(no.direita, indent, true);
     }
-    System.out.println(no.info + "(" + cor + ")");
-
-    imprimirEstruturaRecursivo(no.esquerda, indent, false);
-    imprimirEstruturaRecursivo(no.direita, indent, true);
+    private void transplantar (NoRB u, NoRB v){
+        if(u.pai == null){
+            this.raiz = v;
+        } else if(u == u.pai.esquerda){
+            u.pai.esquerda = v;
+        } else {
+            u.pai.direita = v;
+        }
+        if(v != null){
+            v.pai = u.pai;
+        }
+    }
+    private NoRB encontrarMenorValor(NoRB no){
+        while(no.esquerda != null) {
+            no = no.esquerda;
+        }
+        return no;
+    }
+    private NoRB encontrarNo(int valor) {
+        NoRB atual = this.raiz;
+        while (atual != null) {
+            if (valor == atual.info) {
+                return atual;
+            } else if (valor < atual.info) {
+                atual = atual.esquerda;
+            } else {
+                atual = atual.direita;
+            }
+        }
+        return null; 
+    }
+    private void corrigirRemocao(NoRB x) {
+        while (x != raiz && getCor(x) == Cor.PRETO) {
+            if (x == x.pai.esquerda) {
+                NoRB w = x.pai.direita; 
+                if (getCor(w) == Cor.VERMELHO) {
+                    w.cor = Cor.PRETO;
+                    x.pai.cor = Cor.VERMELHO;
+                    rotacaoEsquerda(x.pai);
+                    w = x.pai.direita; 
+                }
+                if (getCor(w.esquerda) == Cor.PRETO && getCor(w.direita) == Cor.PRETO) {
+                    w.cor = Cor.VERMELHO;
+                    x = x.pai; 
+                } else {
+                    if (getCor(w.direita) == Cor.PRETO) {
+                        w.esquerda.cor = Cor.PRETO;
+                        w.cor = Cor.VERMELHO;
+                        rotacaoDireita(w);
+                        w = x.pai.direita; 
+                    }
+                    w.cor = x.pai.cor;
+                    x.pai.cor = Cor.PRETO;
+                    w.direita.cor = Cor.PRETO;
+                    rotacaoEsquerda(x.pai);
+                    x = raiz; 
+                }
+            } 
+            else {
+                NoRB w = x.pai.esquerda; 
+                if (getCor(w) == Cor.VERMELHO) {
+                    w.cor = Cor.PRETO;
+                    x.pai.cor = Cor.VERMELHO;
+                    rotacaoDireita(x.pai);
+                    w = x.pai.esquerda; 
+                }
+                if (getCor(w.esquerda) == Cor.PRETO && getCor(w.direita) == Cor.PRETO) {
+                    w.cor = Cor.VERMELHO;
+                    x = x.pai; 
+                } else {
+                    if (getCor(w.esquerda) == Cor.PRETO) {
+                        w.direita.cor = Cor.PRETO;
+                        w.cor = Cor.VERMELHO;
+                        rotacaoEsquerda(w);
+                        w = x.pai.esquerda; 
+                    }
+                    w.cor = x.pai.cor;
+                    x.pai.cor = Cor.PRETO;
+                    w.esquerda.cor = Cor.PRETO;
+                    rotacaoDireita(x.pai);
+                    x = raiz; 
+                }
+            }
+        }
+        if (x != null) {
+            x.cor = Cor.PRETO;
+        }
     }
 }
